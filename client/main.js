f43c2e81f07ba21c0b1dbc761d800bac66530998
sambulo <sambulosendas@gmail.com>
2018-12-07 13:55:39 +0000

    hello

--- client/main.js
+++ client/main.js
@@ -1,40 +1,47 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Meteor } from "meteor/meteor";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
+import {Tracker} from 'meteor/tracker';
 
-const players = [
-  {
-    _id: "1",
-    name: "Sambulo",
-    score: 99
-  },
-  {
-    _id: "2",
-    name: "Senda",
-    score: 91
-  },
-  {
-    _id: "3",
-    name: "Michael",
-    score: -13
-  }
-];
+import {Players} from './../imports/api/players';
 
-const renderPlayers = function(playersList) {
-  return playersList.map(function(player) {
-    return <p key={player._id}>{player.name} has {player.score} points</p>;
+const renderPlayers = function (playersList) {
+  return playersList.map(function (player) {
+    return <p key={player._id}>{player.name} has {player.score} point(s).</p>;
   });
 };
 
-Meteor.startup(function() {
-  let title = "Score Store Keep";
-  let name = "Sambulo Senda";
-  let jsx = (
-    <div>
-      <h1>{title}</h1>
-      <p>I am looking at this code</p>
-      {renderPlayers(players)}
-    </div>
-  );
-  ReactDOM.render(jsx, document.getElementById("app"));
+const handleSubmit = function (e) {
+  let playerName = e.target.playerName.value;
+
+  e.preventDefault();
+
+  if (playerName) {
+    e.target.playerName.value = '';
+    Players.insert({
+      name: playerName,
+      score: 0
+    });
+  }
+};
+
+Meteor.startup(function () {
+  Tracker.autorun(function () {
+    let players = Players.find().fetch();
+    let title = 'Score Keep';
+    let name = 'Mike';
+    let jsx = (
+      <div>
+        <h1>{title}</h1>
+        <p>Hello {name}!</p>
+        <p>This is my second p.</p>
+        {renderPlayers(players)}
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="playerName" placeholder="Player name"/>
+          <button>Add Player</button>
+        </form>
+      </div>
+    );
+    ReactDOM.render(jsx, document.getElementById('app'));
+  });
 });
--- server/main.js
+++ server/main.js
@@ -2,9 +2,10 @@ import { Meteor } from "meteor/meteor";
 import { Players } from "./../imports/api/players";
 
 Meteor.startup(function() {
-  Players.insert({
-    name: "Sambusenda",
-    score: 3
-  });
-  console.log(Players.find().fetch());
+
+  let square = function (x) {
+    return x * x;
+  }
+
+  console.log(square(10));
 });
